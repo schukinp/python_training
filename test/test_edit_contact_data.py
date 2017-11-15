8140bbf1aae10052055f272c8deb3a7bdb7abcfb
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2017-11-15 16:27:18 +0300

    Апгрейд теста изменения данных контакта

--- test/test_edit_contact_data.py
+++ test/test_edit_contact_data.py
@@ -7,12 +7,13 @@ def test_edit_contact_data(app, db, check_ui):
     if len(db.get_contact_list_from_db()) == 0:
         app.contact.create(Contact(firstname="Ivan", lastname="Ivanov"))
     old_contacts = db.get_contact_list_from_db()
-    contact = Contact(firstname="Petr", lastname="Petrov")
-    contact.id = random.choice(old_contacts).id
-    app.contact.edit_contact_by_id(contact.id, contact)
+    new_data = Contact(firstname="Petr", lastname="Petrov")
+    contact = random.choice(old_contacts)
+    app.contact.edit_contact_by_id(contact.id, new_data)
     new_contacts = db.get_contact_list_from_db()
     assert len(old_contacts) == len(new_contacts)
-    contact.id = contact
+    index = old_contacts.index(contact)
+    old_contacts[index] = new_data
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
